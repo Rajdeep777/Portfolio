f0626dc230822600d1b88b865a844727c186770e
Rajdeep Singh <74129008+Rajdeep777@users.noreply.github.com>
2023-12-05 21:20:36 +0530

    Update HeroSection.jsx

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -63,7 +63,7 @@ const HeroSection = () => {
           transition={{ duration: 0.5 }}
           className="col-span-4 place-self-center mt-4 lg:mt-0"
         >
-          <div className="rounded-full bg-[#181818] w-[350px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+          <div className="rounded-full bg-[#181818] w-[350px] h-[250px] relative">
             <Image
               src="/images/hero-image.png"
               alt="hero image"
